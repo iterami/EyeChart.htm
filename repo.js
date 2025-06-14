39001d0b286bd5943736875e23b0ca293b6c451a
Jan Hořava <czechinthemail@gmail.com>
2025-06-14 01:40:11 -0700

    simplified property usage

--- repo.js
+++ repo.js
@@ -2,21 +2,21 @@
 
 function randomize(){
     let chart = '';
-    let fontsize = core_storage_data['fontsize'];
+    let fontsize = core_storage_data.fontsize;
 
-    for(let row = 0; row < core_storage_data['rows']; row++){
+    for(let row = 0; row < core_storage_data.rows; row++){
         chart += '<div style="font-size:' + fontsize + 'px">';
 
-        for(let letter = 0; letter < core_storage_data['letter-count']; letter++){
-            chart += core_storage_data['letters'][core_random_integer(core_storage_data['letters'].length)] + ' ';
+        for(let letter = 0; letter < core_storage_data.count; letter++){
+            chart += core_storage_data.letters[core_random_integer(core_storage_data.letters.length)] + ' ';
         }
 
         chart += '</div>';
 
-        fontsize *= core_storage_data['font-decrease'];
+        fontsize *= core_storage_data.decrease;
     }
 
-    core_elements['chart'].innerHTML = chart;
+    core_elements.chart.innerHTML = chart;
 }
 
 function repo_init(){
@@ -28,15 +28,15 @@ function repo_init(){
       },
       'info': '<button id=randomize type=button>Randomize</button>',
       'storage': {
-        'font-decrease': .8,
+        'count': 5,
+        'decrease': .8,
         'fontsize': 150,
-        'letter-count': 5,
         'letters': 'WEMƎ',
         'rows': 20,
       },
-      'storage-menu': '<table><tr><td><input class=mini id=font-decrease step=any type=number><td>Font Decrease'
+      'storage-menu': '<table><tr><td><input class=mini id=decrease step=any type=number><td>Font Decrease'
         + '<tr><td><input class=mini id=fontsize step=any type=number><td>Font Size'
-        + '<tr><td><input class=mini id=letter-count min=1 step=1 type=number><td>Letter Count'
+        + '<tr><td><input class=mini id=count min=1 step=1 type=number><td>Letter Count'
         + '<tr><td><input class=mini id=letters type=text><td>Letters'
         + '<tr><td><input class=mini id=rows min=1 step=1 type=number><td>Rows</table>',
       'title': 'EyeChart.htm',
